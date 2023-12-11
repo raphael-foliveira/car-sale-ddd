726c1336e8df4f218abee589cb4210faa15d68bf
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-12-11 09:30:53 -0300

    fix: car repository should receive entity during creation

--- src/domain/repositories/car.repository.ts
+++ src/domain/repositories/car.repository.ts
@@ -1,10 +1,9 @@
-import { CreateCarDto } from '../../application/dto/car/create-car.dto';
 import { Car } from '../entities/car.entity';
 
 export interface CarRepository {
   findAll(): Promise<Car[]>;
   findById(id: number): Promise<Car>;
-  create(car: CreateCarDto): Promise<Car>;
+  create(car: Car): Promise<Car>;
   delete(id: number): Promise<void>;
   update(car: Car): Promise<Car>;
 }
